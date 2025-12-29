ede4507289b5126961b8f080348951c418275a4d
Jinfeng <jinfeng.wang25@gmail.com>
2025-12-28 16:17:24 -0800

    teleoperation test complete

---
+++ tiago_server/run_tiago_server.sh
@@ -0,0 +1,14 @@
+# activate conda environment
+conda activate tiago
+
+# export env vars
+export PAL_DISTRO=gallium
+export ROS_DISTRO=noetic
+source /opt/pal/${PAL_DISTRO}/setup.bash
+export ROS_MASTER_URI=http://tiago-224c:11311
+export ROS_IP=192.168.0.110
+cd tiago_server
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
+#run node
+python /home/pal/krhinok/SaTaMoma-Exp/tiago_server/tiago_server/tiago_server_node.py
