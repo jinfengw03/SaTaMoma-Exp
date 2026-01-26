53fdca8acba6a0db91621fc03752ffe74118e26c
rohan <tikotekarr@gmail.com>
2026-01-26 13:03:49 -0800

    logs for whiteboard task and gitignore update

---
+++ tiago_client/tiago_client/run_tiago_client.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 1. Activate base conda to use 'conda' commands
+source /home/jiachenli/miniconda3/bin/activate
+
+# 2. Reset and activate the robot-specific environment
+conda deactivate
+source /opt/ros/noetic/setup.bash
+conda activate ros39
+
+# 3. Set existing robot teleop variables
+export TIAGO_TELEOP_TYPE=KEYBOARD
+export ENABLE_VLM=${ENABLE_VLM:-0}  # Set to 1 to enable VLM, 0 to disable (default)
+python tiago_client/run_tiago_real.py
